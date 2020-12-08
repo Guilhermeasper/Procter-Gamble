ec48d41eaaf943b36283f4800559e111f28df82c
Guilherme Afonso <gasp@cin.ufpe.br>
2020-12-07 23:07:39 -0300

    phong ready

--- Projeto 3/script.js
+++ Projeto 3/script.js
@@ -4,91 +4,111 @@ var imgSpecular;
 let arrayOfBase;
 let arrayOfNormal;
 let arrayOfSpecular;
+let cameraZ = 10;
+let lightZ = 300;
+let observerMatrix;
+let lightMatrix;
+let rangeLight;
+let onlySpecular = false;
+let onlyDifuse = false;
 
 function preload() {
   imgBase = loadImage("./Texturas/char2_d.png");
   imgNormal = loadImage("./Texturas/char2_n.png");
   imgSpecular = loadImage("./Texturas/char2_s.png");
-  arrayOfBase = pixelsToArray(imgBase);
-  arrayOfNormal = pixelsToArray(imgNormal);
-  arrayOfSpecular = pixelsToArray(imgSpecular);
+  
 }
 
 function setup() {
   createCanvas(410, 736);
+  observerMatrix = generateObserverMatrix();
+  
 
-  arrayOfImage = pixelsToArray();
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      const element = arrayOfImage[x][y];
-      set(x, y, element);
-    }
-  }
-  updatePixels();
+  image(imgNormal, 0, 0);
+  loadPixels();
+  arrayOfNormal = pixelsToArray(imgNormal);
+
+  image(imgSpecular, 0, 0);
+  loadPixels();
+  arrayOfSpecular = pixelsToArray(imgSpecular);
+  
+
+  image(imgBase, 0 , 0);
+  loadPixels();
+  arrayOfBase = pixelsToArray(imgBase);
+  print("Ready");
+  lightMatrix = generateLightMatrix(0, 0);
+  phongLight();
+  
 }
+
+
+
+
 
 function pixelsToArray(img) {
   let newMatrix = [];
   for (let x = 0; x < width; x++) {
     let temp = []
     for (let y = 0; y < height; y++) {
-      temp.push(img.get(x, y));
+      let currentRGB = normalizeColor(img.get(x, y));
+      let r = currentRGB[0];
+      let g = currentRGB[1];
+      let b = currentRGB[2];
+
+      let v = createVector(r, g, b);
+      temp.push(v);
     }
     newMatrix.push(temp);
   }
   return newMatrix;
-
-
-  // let arrayOfPixels = [];
-  // let x = 0;
-  // let tempArray = [];
-  // for (let i = 0; i < pixels.length; i += 4) {
-  //   if (x == width) {
-  //     arrayOfPixels.push(tempArray);
-  //     tempArray = [];
-  //     x = 0;
-  //   }
-  //   r = pixels[i];
-  //   g = pixels[i + 1];
-  //   b = pixels[i + 2];
-  //   a = pixels[i + 3];
-  //   let newArray = [r, g, b, a];
-  //   tempArray.push(newArray);
-  //   x++;
-  // }
-  // return transformMatrix(arrayOfPixels);
 }
 
-function transformMatrix(matrix) {
-  let newMatrix = new Array(matrix[0].length).fill([]);
-  for (let i = 0; i < matrix[0].length; i++) {
-    for (let j = 0; j < matrix.length; j++) {
-      newMatrix[i][j] = matrix[j][i];
+function phongLight(){
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let ambientLight = createVector(0, 0, 0);
+
+      rangeLight = createVector(0.7, 0.7, 0.7);
+      let difuseLight = createVector(0, 0, 0);
+      let normalAndLight = Math.max(arrayOfNormal[x][y].dot(lightMatrix[x][y]), 0);
+      p5.Vector.mult(arrayOfBase[x][y], normalAndLight, difuseLight);
+      
+      let mirrorMath = createVector(0, 0, 0);
+      let mirrorLight = createVector(0, 0, 0);
+      p5.Vector.mult(arrayOfNormal[x][y], 2*normalAndLight, mirrorMath);
+      p5.Vector.sub(lightMatrix[x][y], mirrorMath, mirrorLight);
+
+      let specularMath = Math.max(observerMatrix[x][y].dot(mirrorLight), 0)**5;
+      let specularLight = createVector(0, 0, 0);
+      p5.Vector.mult(arrayOfSpecular[x][y], specularMath, specularLight);
+
+      let difuseAndSpecular = createVector(0, 0, 0);
+      let finalMath = createVector(0, 0, 0);
+      if(onlySpecular){
+        p5.Vector.add(createVector(0, 0, 0), specularLight, difuseAndSpecular);
+      }else if(onlyDifuse){
+        p5.Vector.add(difuseLight, createVector(0, 0, 0), difuseAndSpecular);
+      }else{
+        p5.Vector.add(difuseLight, specularLight, difuseAndSpecular);
+      }
+      p5.Vector.mult(rangeLight, difuseAndSpecular, finalMath)
+
+      
+      let finalLight = createVector(0, 0, 0);
+      
+      p5.Vector.add(finalMath, ambientLight, finalLight);
+      
+      let color = [finalLight.x, finalLight.y, finalLight.z, 1];
+      set(x, y, denormalizeColor(color));
     }
   }
-  return newMatrix;
+  updatePixels();
 }
 
-function draw() {
-  // background(0);
-  // image(img1s, 0, 0)
-  // image(img1d, 0, 0)
-  // loadPixels();
-  // let d = pixelDensity();
-  // let halfImage = 4 * (width * d) * ((height) * d);
-  // for (let i = 0; i < halfImage; i += 4) {
-  //   pixels[i] = red(0);
-  //   pixels[i + 1] = green(0);
-  //   pixels[i + 2] = blue(0);
-  //   pixels[i + 3] = alpha(0);
-  // }
-  // updatePixels();
-}
-
-function mouseClicked() {
-  print(mouseX, mouseY);
-  //print(arrayOfImage[0]);
-  print(arrayOfImage[mouseX][mouseY]);
+function mouseMoved(){
+  lightMatrix = generateLightMatrix(mouseX, mouseY);
+  phongLight();
 }
 
 function normalizeColor(color) {
@@ -103,4 +123,49 @@ function normalizeColor(color) {
   a = a / 255;
 
   return [r, g, b, a];
+}
+
+function denormalizeColor(color){
+  let r = color[0];
+  let g = color[1];
+  let b = color[2];
+  let a = color[3];
+
+  r = r * 255;
+  g = g * 255;
+  b = b * 255;
+  a = a * 255;
+
+  return [r, g, b, a];
+}
+
+
+function generateObserverMatrix(){
+  let newMatrix = [];
+  let cameraX = width;
+  let cameraY = height;
+  for (let x = 0; x < width; x++) {
+    let temp = [];
+    for (let y = 0; y < height; y++) {
+      let v = createVector(Math.abs(cameraX - x), Math.abs(cameraY - y), cameraZ);
+      temp.push(v.normalize());
+    }
+    newMatrix.push(temp);
+  }
+  return newMatrix;
+}
+
+function generateLightMatrix(x, y){
+  let newMatrix = [];
+  let LightX = x;
+  let LightY = y;
+  for (let x = 0; x < width; x++) {
+    let temp = [];
+    for (let y = 0; y < height; y++) {
+      let v = createVector(Math.abs(LightX - x), Math.abs(LightY - y), lightZ);
+      temp.push(v.normalize());
+    }
+    newMatrix.push(temp);
+  }
+  return newMatrix;
 }
